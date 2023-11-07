b4e0a52d7968d5da4890b31fd95509606c30e100
Thijs Luyten <99009420+DeltionThijs@users.noreply.github.com>
2023-11-07 21:03:05 +0100

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,15 +1,1 @@
 
-  git config --global user.email "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@github.com"
-  git config --global user.name "DeltionThijs"
-
-
-git remote set-url origin https://DeltionThijs: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@github.com/DeltionThijs/Stationzuil.git
-
-
-git add .
-
-git commit -m "Stationzuil V1"
-
-git push origin master
-
-
