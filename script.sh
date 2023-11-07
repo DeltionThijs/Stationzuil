36c13c9ce810e1d866e7e8e8c3c74c17733f4acd
Thijs Luyten <99009420+DeltionThijs@users.noreply.github.com>
2023-11-07 20:59:13 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
-
-
-git init
-git remote set-url origin https://DeltionThijs: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@github.com/DeltionThijs/Stationzuil.git
-
-git add .
-
-git commit -m "Stationzuil V1"
-
-git push origin master  # Dit is voor de standaard branch 'master'.
-  
 
 
