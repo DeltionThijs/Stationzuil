dd247d0679517c04a641ff755125d43b53a12201
DeltionThijs <thijs.luyten@outlook.com>
2023-11-07 20:51:15 +0100

    Stationzuil V1

--- commit.sh
+++ commit.sh
@@ -1,4 +1,8 @@
 
+  git config --global user.email "thijs.luyten@outlook.com"
+  git config --global user.name "DeltionThijs"
+
+
 git remote set-url origin https://DeltionThijs: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@github.com/DeltionThijs/Stationzuil.git
 
 
